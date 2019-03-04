8c26c6ce376ce1e455fed0e33af8541a471c27e8
jplacensia <jplace28@gmail.com>
2019-03-03 17:20:46 -0700

    Create Javascript.js

---
+++ Javascript.js
@@ -0,0 +1,17 @@
+var bossName = 'Kim';
+var bossName2 = 'Nina';
+
+switch (bossName) {
+case 'Kim':
+  console.log('Hello Boss');
+  break;
+
+case 'Nina':
+  console.log('Hello Boss');
+  break;
+
+default:
+  console.log('Hi there');
+  break;
+
+}
